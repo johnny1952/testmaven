577c488afc065d433e440a8c62dd6299f1101652
JohnnyLi <JohnnyLi@LAPTOP-PFQ7T85U>
2018-01-12 15:57:40 +0800

    first

---
+++ src/main/java/com/johnnyli/App.java
@@ -0,0 +1,13 @@
+package com.johnnyli;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JSONObject jo=new JSONObject();
+		jo.toJSONString();
+	}
+
+}
